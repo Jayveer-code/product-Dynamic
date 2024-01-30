de5f3e6f840d4f788e44225b71fe2b8c4673340d
Jayveer-code <jayuchavda444@gmail.com>
2024-01-30 05:59:07 -0800

    Initial commit of Django project

---
+++ Hmart/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def Main(request):
+    return render(request,'index.html')
+
+def LoginPage(request):
+    return render(request,'login.html')
+
+
+def ContactPage(request):
+    return render(request,'contact.html')
+
+def AboutPage(request):
+    return render(request,'about.html')
+
+def AccountPage(request):
+    return render(request,'my-account.html')
+
